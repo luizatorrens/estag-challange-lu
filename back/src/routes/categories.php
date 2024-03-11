01137a04c9b8356e191c2a59851db2a641570443
luiza <luiza.torrenss@gmail.com>
2024-03-11 17:20:32 -0300

    third commit

--- back/src/routes/categories.php
+++ back/src/routes/categories.php
@@ -1,7 +1,7 @@
 <?php 
 header ('Access-Control-Allow-Origin: *');
 header ('Access-Control-Allow-Methods: GET, POST, DELETE');
-header ('Access-Control-Allow-Headers: X-Requested-With');
+header ('Access-Control-Allow-Headers: *');
 include "../services/categoriesServices.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
